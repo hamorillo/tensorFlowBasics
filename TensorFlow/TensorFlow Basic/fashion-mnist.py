5ceef323eb1c8084ecb86ebd505348e54a5aff62
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2018-07-27 11:51:06 +0200

    Example from tensorflow.org with Fashion MNIST dataset

---
+++ TensorFlow/TensorFlow Basic/fashion-mnist.py
@@ -0,0 +1,68 @@
+# Example from: https://www.tensorflow.org/tutorials/keras/basic_classification
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+# Retrieve the dataset
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+
+# Preprocess de data, both, training and testing
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Setup the layers of the model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+# Compile the model
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5)
+
+# Evaluate accuracy
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
+
+# Make predictions with the test data
+predictions = model.predict(test_images)
+
+# Print the predictions in comparison with the real label
+print(predictions)
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions[i])
+    true_label = test_labels[i]
+    if predicted_label == true_label:
+      color = 'green'
+    else:
+      color = 'red'
+    plt.xlabel("{} ({})".format(class_names[predicted_label],
+                                  class_names[true_label]),
+                                  color=color)
+plt.show()
